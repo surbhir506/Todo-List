d21ea9ce721a63b648f597ec4e9fc62732337683
surbhir506 <surbhir506@gmail.com>
2024-04-28 01:00:26 +0530

    commit

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1 align="center" id="title">TODO</h1>
+
+<p id="description">Todo app Build using MERN Stack. Allowing to Creating Todo list, Update and Delete Todo list within app.</p>
+
+  
+  
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+*   Add Todo
+*   get Todo list
+*   Update Status
+*   Delete Todo
+
+
+<h2>🛠️ Installation Steps:</h2>
+
+<p>1. cd Server</p>
+
+<p>2. Install dependencies for Server</p>
+
+```
+npm install
+```
+
+<p>3. Start Server</p>
+
+```
+npm start
+```
+
+<p>4. Open Separate terminal for Client</p>
+
+<p>5. cd Client</p>
+
+<p>6. Install dependencies for Client</p>
+
+```
+npm install
+```
+
+<p>7. Run Client</p>
+
+```
+npm run dev
+```
+
+  
+<h2>💻 Built with</h2>
+
+Technologies used in the project:
+
+*   ReactJs
+*   NodeJs
+*   Mongodb
+*   ExpressJS
+*   Tailwind CSS
+
+
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Todo = require('./model/Todo')
+const PORT = 4000;
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(console.log("Database is Connected"))
+    .catch(err => console.log(err))
+
+app.get('/', async (req, res) => {
+    try {
+        const data = await Todo.find()
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.put("/update/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const up = await Todo.findByIdAndUpdate({ _id: id }, { complete: true })
+        res.json(up)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.post("/add", async (req, res) => {
+    try {
+        const { name, description } = req.body;
+        const data = await Todo.create({
+            name: name,
+            description: description
+        })
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.delete('/delete/:id', async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const del = await Todo.findOneAndDelete({_id: id})
+        res.json(del)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
